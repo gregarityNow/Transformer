6566f7d36931520e7565091e46514bb64fcd790d
felix <felix.herron@gmail.com>
2022-08-09 13:37:16 +0200

    train_modelFEH

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 print("beginn ja")
-# from .Batch import *
-# from .Process import *
-# print("letrain")
+from .Batch import *
+from .Process import *
+print("letrain")
 # from .train import train_modelFEH
-# print("letrain2")
-# from .Models import *
-# from .Tokenize import CamTok
+print("letrain2")
+from .Models import *
+from .Tokenize import CamTok
