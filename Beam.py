9a6eef6728253a30df826783b138b018dda74590
felix <felix.herron@gmail.com>
2022-08-23 01:19:54 +0200

    legno

--- Beam.py
+++ Beam.py
@@ -80,7 +80,7 @@ def beam_search(src, model, SRC, TRG, opt):
                 sentence_lengths[i] = vec[1] # Position of first end symbol
 
         num_finished_sentences = len([s for s in sentence_lengths if s > 0])
-        print("numster",num_finished_sentences)
+        print("numster",num_finished_sentences, "legno",sentence_lengths)
         if num_finished_sentences == opt.k:
             alpha = 0.7
             div = 1/(sentence_lengths.type_as(log_scores)**alpha)
