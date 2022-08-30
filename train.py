7645d4ad0ec70643a63e1cc7ca5c71f865547250
felix <felix.herron@gmail.com>
2022-08-30 11:07:39 +0200

    gregarity now

--- train.py
+++ train.py
@@ -452,6 +452,7 @@ def mainFelixCamemLayer():
 
         #finetune on neonyms
         df = read_data_felix(opt, allTerms=False)
+        opt.train, opt.valid = create_dataset(opt, SRC, TRG)
         opt.optimizer = torch.optim.Adam(model.parameters(), lr=opt.lr*0.1, betas=(0.9, 0.98), eps=1e-9)
         if opt.SGDR == True:
             opt.sched = CosineWithRestarts(opt.optimizer, T_max=opt.train_len)
